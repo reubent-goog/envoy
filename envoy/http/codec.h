4ca721c0329a9fe9ac9a0d5d87e997bea60a7460
Reuben Tanner <reubent@google.com>
2025-12-18 16:22:01 -0500

    expose codec level stream id
    Signed-off-by: Reuben Tanner <reubent@google.com>

--- envoy/http/codec.h
+++ envoy/http/codec.h
@@ -463,6 +463,16 @@ public:
    * Get the bytes meter for this stream.
    */
   virtual const StreamInfo::BytesMeterSharedPtr& bytesMeter() PURE;
+
+  /**
+   * @return absl::optional<uint32_t> the codec level stream ID if available
+   * or nullopt if not applicable or not yet assigned.
+   *
+   * HTTP/1 streams return nullopt.
+   * HTTP/2 streams return the HTTP/2 stream ID.
+   * HTTP/3 streams are not implemented yet.
+   */
+  virtual absl::optional<uint32_t> codecStreamId() const PURE;
 };
 
 /**
